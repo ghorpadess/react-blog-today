82999aea6032c3046594e003df3f44cdb8185a00
ghorpadess <swapnilghorpade5567@gmail.com>
2023-12-16 19:01:41 +0530

    fixed console errors

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+
+// eslint-disable-next-line no-unused-vars
+function Logo({ width = "100px" }) {
+  return <div>Logo</div>;
+}
+
+export default Logo;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import store from "./store/store.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
